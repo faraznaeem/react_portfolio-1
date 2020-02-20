f0cc6b0beac8812a4eb8940cd60876cdbe46f221
faraznaeem <faraznaeem@gmail.com>
2020-02-20 14:52:23 +0100

    removes typical

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -9,19 +9,7 @@ const Hello = () =>
           <div className="title">
             <h1 id="hello">Hi, I'm Kayla Woobury</h1>
             <p>A
-              <Typical
-                loop={Infinity}
-                wrapper='i'
-                steps={[
-                  'n aspiring front end developer',
-                  2000,
-                  'n aspiring back end developer',
-                  2000,
-                  ' nature enthusiast',
-                  2000,
-                  ' traveller',
-                  2000
-                ]}
+          
               />
             </p>
           </div>
